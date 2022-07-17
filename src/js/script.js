af43b21b8c4624bde6ea22d1127270b5482c03a9
trevinshu <trevinshu2008@gmail.com>
2022-07-16 23:35:05 -0600

    added smooth scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,9 @@ const menu = document.getElementById('menu');
 burgerBtn.addEventListener('click', (e) => {
   menu.classList.toggle('showMenu');
   if (menu.classList.contains('showMenu')) {
-    menu.classList.add('animate__animated', 'animate__slideInLeft');
+    menu.classList.add('animate__animated', 'animate__slideInLeft', 'animate__fast');
   } else {
-    menu.classList.remove('animate__slideInLeft');
+    menu.classList.remove('animate__slideInLeft', 'animate__fast');
   }
   e.preventDefault();
 });

2b8fb8fc4b509ab39236d2ff64009422cca4b77f
trevinshu <trevinshu2008@gmail.com>
2022-07-16 23:05:09 -0600

    added animation to hamurger menu opening

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,10 @@ const burgerBtn = document.getElementById('burgerBtn');
 const menu = document.getElementById('menu');
 burgerBtn.addEventListener('click', (e) => {
   menu.classList.toggle('showMenu');
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.add('animate__animated', 'animate__slideInLeft');
+  } else {
+    menu.classList.remove('animate__slideInLeft');
+  }
   e.preventDefault();
 });

b2d5269f3baa76ca7406453327bf8f9312d8205d
trevinshu <trevinshu2008@gmail.com>
2022-07-20 22:45:18 -0600

    added resume

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,5 @@ const burgerBtn = document.getElementById('burgerBtn');
 const menu = document.getElementById('menu');
 burgerBtn.addEventListener('click', (e) => {
   menu.classList.toggle('showMenu');
-  if (menu.classList.contains('showMenu')) {
-    menu.classList.add('animate__animated', 'animate__slideInLeft', 'animate__fast');
-  } else {
-    menu.classList.remove('animate__slideInLeft', 'animate__fast');
-  }
   e.preventDefault();
 });
